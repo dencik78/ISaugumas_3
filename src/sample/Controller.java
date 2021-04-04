cdda70f48910c7807884050a3fa67809cd3d4385
Denis Martinkevic <conters@bk.ru>
2021-04-04 17:30:44 +0300

    New Project
    Create new Project and create interface

---
+++ src/sample/Controller.java
@@ -0,0 +1,41 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.TextArea;
+
+public class Controller {
+
+    @FXML
+    private TextArea textArea;
+
+    @FXML
+    private TextArea keyArea;
+
+    @FXML
+    private CheckBox enCheckBox;
+
+    @FXML
+    private CheckBox deCheckBox;
+
+    @FXML
+    private Button startButton;
+
+    @FXML
+    void StartButtonClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void deCheckBoxClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void enCheckBoxClick(ActionEvent event) {
+
+    }
+
+}
